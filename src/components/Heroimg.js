a93ad20057c689f0fd91b3f621d7d884e57ac796
Akhil Gupta <ag7223836938@gmail.com>
2023-03-14 20:59:43 +0530

    Photo-changed

--- src/components/Heroimg.js
+++ src/components/Heroimg.js
@@ -2,6 +2,7 @@ import "./HeroStyles.css";
 
 import React from 'react'
 import IntroImg from '../assets/bgimg.jpg'
+import photo from '../assets/photo.jpeg'
 import { Link } from "react-router-dom";
 const Heroimg = () => {
   return (
@@ -10,8 +11,9 @@ const Heroimg = () => {
         <img className="into-img" src={IntroImg} alt="IntroImg"/>
     </div>
     <div className="content">
-        <p>HI, I'M A Software Developer.</p>
-        <h1>MERN Developer.</h1>
+        <img src={photo}/>
+        {/* <p>HI, I'M A Software Developer.</p> */}
+        <h1>Software Developer.</h1>
         <div>
             <Link to="/project" className="btn">Projects</Link>
             <Link to="/contact" className="btn btn-light">Contact</Link>
